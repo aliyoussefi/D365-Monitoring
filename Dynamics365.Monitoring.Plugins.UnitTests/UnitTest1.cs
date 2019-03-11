942c02ee89705b9d84844f1e0d02bd115b543760
Ali Youssefi <alyousse@microsoft.com>
2019-03-11 09:00:56 -0500

    added plugin and plugin unit tests proj

---
+++ Dynamics365.Monitoring.Plugins.UnitTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Dynamics365.Monitoring.Plugins.UnitTests {
+    [TestClass]
+    public class UnitTest1 {
+        [TestMethod]
+        public void TestMethod1() {
+        }
+    }
+}
