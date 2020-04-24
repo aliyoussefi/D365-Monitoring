6302956d582fb3306f8a5a4022b8e0e5dc1c5221
Ali Youssefi <alyousse@microsoft.com>
2020-04-24 13:38:38 -0500

    added unit test for customevent

--- Dynamics365.Monitoring.Plugins.UnitTests/Ready2019TestPluginPushToAppInsights.cs
+++ Dynamics365.Monitoring.Plugins.UnitTests/Ready2019TestPluginPushToAppInsights.cs
@@ -19,9 +19,17 @@ namespace Dynamics.Ready.Plugins.UnitTests {
             return fakedContext;
         }
 
+        public static string _instrumentationKey;
+
+        [ClassInitialize]
+        public static void ClassInit(TestContext context)
+        {
+            _instrumentationKey = "";
+        }
+
         [TestMethod]
         public void TestApplicationInsightsPostCreate() {
-            string unsecureConfig = @"<Settings><setting name=""instrumentationKey""><value>MyApplicationInsightsKey</value></setting></Settings>";
+            string unsecureConfig = @"<Settings><setting name=""instrumentationKey""><value>" + _instrumentationKey + "</value></setting></Settings>";
             string secureConfig = null;
             var fakedContext = new XrmFakedContext();
             var wfContext = fakedContext.GetDefaultPluginContext();
@@ -38,6 +46,29 @@ namespace Dynamics.Ready.Plugins.UnitTests {
             wfContext.InputParameters.Add(new KeyValuePair<string, object>("Target", accountEntity));
 
             var result = fakedContext.ExecutePluginWithConfigurations<ApplicationInsightsTrackException>(wfContext, unsecureConfig, secureConfig);
+
+        }
+
+        [TestMethod]
+        public void TestApplicationInsightsTrackCustomEvent()
+        {
+            string unsecureConfig = @"<Settings><setting name=""instrumentationKey""><value>" + _instrumentationKey + "</value></setting></Settings>";
+            string secureConfig = null;
+            var fakedContext = new XrmFakedContext();
+            var wfContext = fakedContext.GetDefaultPluginContext();
+            wfContext.MessageName = "Create";
+            wfContext.Stage = 40;
+            wfContext.CorrelationId = new Guid("b374239d-4233-41a9-8b17-a86cb4f737b5");
+            Entity accountEntity = new Entity();
+            accountEntity.Attributes = new AttributeCollection();
+            accountEntity.Attributes.Add(new KeyValuePair<string, object>("branch", "Some Branch"));
+            accountEntity.Attributes.Add(new KeyValuePair<string, object>("parentaccountid", new EntityReference("parentaccount", null)));
+            //((EntityReference)accountEntity.Attributes["parentaccountid"]).Id = null;
+
+            wfContext.InputParameters = new ParameterCollection();
+            wfContext.InputParameters.Add(new KeyValuePair<string, object>("Target", accountEntity));
+
+            var result = fakedContext.ExecutePluginWithConfigurations<ApplicationInsightsTrackCustomEvent>(wfContext, unsecureConfig, secureConfig);
 
         }
     }
